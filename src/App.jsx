5ea5d5d66db872bc3a36411a1124d02956cfa17d
christopher7777777 <pokharelchristopher@gmail.com>
2024-07-16 11:29:37 +0545

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import LandingPage from "./component/LandingPage";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Signin from "./auth/Signin"
+import Signup from "./auth/Signup"
+import HomePage from "./components/HomePage"
+import LandingPage from "./components/LandingPage"
+// import UserLayout from "./Layout/UserLayout"
 function App() {
-
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage/>}/>
-
-        
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Signin" element={<Signin/>} />
+        <Route path="/Signup" element={<Signup/>}/>
+        {/* <Route path="/userLayout" element={<UserLayout/>}> */}
+        <Route path="/HomePage" element={<HomePage/>}/>
+        {/* </Route> */}
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/elements/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <div>Footer
+        <div className="flex flex-col items-center justify-center">
+          </div>
+      </div>
+    )
+  }
+  
+  export default Footer
